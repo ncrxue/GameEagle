8e95de873697f90b1127790b0299b5fbec58f4c9
ncrxue <nxue2004@gmail.com>
2022-11-05 14:51:24 -0700

    color text!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
+
     cout << "This is our project, Game eagle for CutieHack 2022." << endl;
+    cout << "\x1B[93mTexting\033[0m" << endl;
+    
 
     return 0;
 }
