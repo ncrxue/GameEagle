da418d9334f958be7973f0b1bace78e148b9789d
jchun <jonathanchunemail@gmail.com>
2022-11-05 20:07:44 -0700

    creative_combat_final

--- creative_combat.cpp
+++ creative_combat.cpp
@@ -240,7 +240,7 @@ void combat(string n,int a1, int a2, int b, int health, int hval)
                     stamused = true;
                     if (ehp <= 0)
             {
-                cout << ename << " Has been defeated by" << name << endl;
+                
                 fighting = false;
                 break;
 
@@ -255,14 +255,14 @@ void combat(string n,int a1, int a2, int b, int health, int hval)
                     stamused = true;
                     if (ehp <= 0)
             {
-                cout << ename << " Has been defeated by" << name << endl;;
+                
                 fighting = false;
                 break;
             }
                 }
                 
             }
-            if (selection == 2)
+            else if (selection == 2)
             {
                 cout << "Enter 1 to block for " << block << endl;
                 cout << "Enter 2 to heal for " << healval << " at the cost of 12 Stam. " << endl;
@@ -339,13 +339,22 @@ void combat(string n,int a1, int a2, int b, int health, int hval)
             stam +=10;
         }
         round ++;
-        
-        }
         if (hp <= 0)
         {
+            fighting = false;
             playing = false;
             cout << name << " has been defeated by " << ename << " Game over!" << endl << "Score: " << pts;
-            break;
+            
+        }
+        else if (ehp <= 0)
+        {
+            fighting = false;
+            cout << ename << " has been defeated by " << name << endl;
+            
+        }
+        }
+        
+
         }
         round = 0;
         hp = hpmax;
@@ -353,6 +362,7 @@ void combat(string n,int a1, int a2, int b, int health, int hval)
         atk2 = atk2max;
         stam = 15; 
         level++;
+        pts++;
         if (level == 2)
         {
             cout << "Congrats, you beat the final boss, your score is: " << pts << endl;
@@ -365,7 +375,7 @@ void combat(string n,int a1, int a2, int b, int health, int hval)
         ename = names[(rand()%3)+(level*3)];
         eatk = 5 + (rand()%3 + (level*3));
         cout << ename << " Attacks, " << name << " get ready to fight!" << endl;
-        playing = true;
+        fighting = true;
         
     }
     }    
